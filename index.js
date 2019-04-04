89d402d5f0c285626a267070f961c646e7beedbd
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-04-04 23:24:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!"
-  
+ return 'Happy holidays, ${name}';
 }
-happyHolidaysTo(You)
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`;
+}
